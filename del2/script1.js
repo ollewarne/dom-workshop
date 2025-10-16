f07c45085fe44361b2a5dd38721b6f27c4a9c3ce
Idris <macbookpro@Idriss-MacBook-Pro.local>
2025-10-16 11:46:52 +0200

    övning 1 klar

---
+++ del2/script1.js
@@ -0,0 +1,21 @@
+// driver Idris
+// Navigator Olle och Axel.
+// color #348AA7
+
+const paragraf = document.querySelector('p');
+
+const changeColor = document.getElementById('buttonOne');
+changeColor.addEventListener('click', () => {
+    paragraf.style.background = '#348AA7';
+})
+
+const addText = document.getElementById('buttonTwo');
+addText.addEventListener('click', () => {
+    paragraf.textContent = paragraf.textContent + ' Hejsan spesan tejsan.';
+})
+
+const removeText = document.getElementById('buttonThree');
+removeText.addEventListener('click', () => {
+     // paragraf.remove();
+     paragraf.textContent = '';
+})
