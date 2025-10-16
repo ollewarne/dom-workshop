67d10e10d2d4501dec4b5253226b71e35daecf85
Lindsteez <lindgren.axel@hotmail.com>
2025-10-16 09:49:30 +0200

    övning 1

---
+++ del1/script1.js
@@ -0,0 +1,20 @@
+// Navigator: Axel
+// Driver: Olle & Idris
+
+const titel = document.createElement('h2');
+titel.textContent = "Min rubrik för dagen";
+titel.style.color = "#1e3a8a";
+
+const paragraf = document.createElement('p');
+paragraf.textContent = "Helt okej väder idag";
+paragraf.style.color = "#fef3c7";
+
+const box = document.createElement('div');
+box.textContent = "Det här är en färgad ruta";
+box.style.background = "#10b981";
+
+document.body.appendChild(titel)
+document.body.appendChild(paragraf)
+document.body.appendChild(box)
+
+paragraf.textContent = "16 oktober"
